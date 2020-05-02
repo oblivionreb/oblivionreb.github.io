9ce87abe19fa9b5eedbebf7189fbc60ff66690d7
oblivionreb <64311931+oblivionreb@users.noreply.github.com>
2020-05-02 19:19:47 -0400

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,20 +1,7 @@
 
 
 // IOS style bug fix
-var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-var $iosSelectRow = document.getElementById("selectRow");
-var $iosFormRow = document.getElementById("formRow");
-var $iosStateSelect  = document.getElementById("state-select");
-var $iosStatusSelect  = document.getElementById("status-select");
-var $iosLabels   = document.getElementsByName("label");
-if(iOS){
-$iosSelectRow.addClass("ios-select-row");
-$iosStateSelect.addClass("ios-state-select");
-$iosStatusSelect.addClass("ios-status-select"); 
-$iosLabels[0].addClass("ios-label");
-$iosLabels[1].addClass("ios-label");
-$iosFormRow.addClass("ios-form-row");
-}
+
 
 window.mobileCheck = function() {
   let check = false;
@@ -314,6 +301,20 @@ console.log(this.config.data.total)
 
 $(document).ready(function(){
 //bracket[0] = min income, bracket[1] = max income, bracket[2] = tax 
+ var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+var $iosSelectRow = document.getElementById("selectRow");
+var $iosFormRow = document.getElementById("formRow");
+var $iosStateSelect  = document.getElementById("state-select");
+var $iosStatusSelect  = document.getElementById("status-select");
+var $iosLabels   = document.getElementsByName("label");
+if(iOS){
+$iosSelectRow.addClass("ios-select-row");
+$iosStateSelect.addClass("ios-state-select");
+$iosStatusSelect.addClass("ios-status-select"); 
+$iosLabels[0].addClass("ios-label");
+$iosLabels[1].addClass("ios-label");
+$iosFormRow.addClass("ios-form-row");
+}
 function taxCalc(bracket, income){
    var total = 0;
     for(var key in bracket){
